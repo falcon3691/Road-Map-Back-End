ceeaf0483f94fd0c357a7ac05387340d40c51923
Oğuzhan Çakar <cakaroguzhan53@gmail.com>
2025-02-27 23:58:05 +0300

    Task 3 final

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,5 +1,6 @@
 import json
 import os
+import datetime
 
 class ExpenseTracker:
     def __init__(self, filename='expenses.json'):
@@ -22,7 +23,10 @@ class ExpenseTracker:
         data = input("Please enter the new expense (amount, description, category): ").split(",", maxsplit=2)
         amount, description, category = (data + ["", "", ""])[:3] 
         try:
-            amount = float(amount.strip())
+            if float(amount) > 0.0:
+                amount = float(amount.strip())
+            else:
+                amount = 0.0
         except ValueError as e:
             amount = 0.0
         description, category = description.strip(), category.strip()
@@ -34,29 +38,72 @@ class ExpenseTracker:
             "id": len(self.expenses) + 1,
             "amount": amount,
             "description": description,
-            "category": category
+            "category": category,
+            "createdAt": datetime.datetime.now().strftime("%d/%m/%Y")
         }
         self.expenses.append(expense)
         self.save_data()
         os.system("cls")
         print(f"New expense with the ID {len(self.expenses)} is added.")
         print("New List\n" + "-"*50)
-        tracker.list_expenses()
+        tracker.list_expenses("")
 
-    def list_expenses(self):
+    def list_expenses(self, data):
         if not self.expenses:
             print("No expense added yet.")
             return
         totalAmount = 0
-        print("\nID  | Amount | Category      | Description")
-        print("-"*50)
-        for expense in self.expenses:
-            try:
-                totalAmount += expense['amount']
-            except ValueError as e:
-                totalAmount += 0
-            print(f"{expense['id']:3} | {expense['amount']:6} | {expense['category']:<13} | {expense['description']}")
-        print("-"*50 + f"\nTotal amount spent: {totalAmount}")
+        if data == "":
+            Data = ""
+        else:
+            Data = input("By Month(1) / By Category(2) / All(Enter)\nPlease chose a filter option: ")
+            
+        if Data == "1":
+            filter = input("Which month do you want to filter by: ")
+            if filter != "":
+                print("\nID  |  Date      | Amount | Category      | Description")
+                print("-"*75)
+                for expense in self.expenses:
+                    date_str = expense["createdAt"]  # JSON'dan gelen tarih verisi
+                    date_obj = datetime.datetime.strptime(date_str, "%d/%m/%Y")  # Tarihi parçala
+                    month = date_obj.month
+                    if month == int(filter):
+                        try:
+                            totalAmount += expense['amount']
+                        except ValueError as e:
+                            totalAmount += 0
+                        print(f"{expense['id']:3} | {expense['createdAt']} | {expense['amount']:6} | {expense['category']:<13} | {expense['description']}")
+                        print("-"*75 + f"\nTotal amount spent: {totalAmount}")
+            else:
+                print("Error! No month value to filter by.")
+                tracker.list_expenses("")
+            
+        elif Data == "2":
+            filter = input("Which category do you want to filter by: ")
+            if filter != "":
+                print("\nID  |  Date      | Amount | Category      | Description")
+                print("-"*75)
+                for expense in self.expenses:
+                    if expense["category"] == filter:
+                        try:
+                            totalAmount += expense['amount']
+                        except ValueError as e:
+                            totalAmount += 0
+                        print(f"{expense['id']:3} | {expense['createdAt']} | {expense['amount']:6} | {expense['category']:<13} | {expense['description']}")
+                        print("-"*75 + f"\nTotal amount spent: {totalAmount}")
+            else:
+                print("Error! No category value to filter by.")
+                tracker.list_expenses("")
+        elif Data == "":
+            print("\nID  |  Date      | Amount | Category      | Description")
+            print("-"*75)
+            for expense in self.expenses:
+                try:
+                    totalAmount += expense['amount']
+                except:
+                    totalAmount += 0
+                print(f"{expense['id']:3} | {expense['createdAt']} | {expense['amount']:6} | {expense['category']:<13} | {expense['description']}")
+            print("-"*75 + f"\nTotal amount spent: {totalAmount}")
 
     def update_expense(self):
         if not self.expenses:
@@ -65,27 +112,41 @@ class ExpenseTracker:
             return
         
         os.system("cls")
-        print("Old List\n" + "-"*50)
-        tracker.list_expenses()
+        print("Old List\n" + "-"*75)
+        tracker.list_expenses("")
         data = input("Please enter the values to update (id, amount, description, category): ").split(",", maxsplit=3)
         id, amount, description, category = (data + ["", "", ""])[:4] 
         try:
             expense_id = int(id.strip())
-            try:
-                amount = float(amount.strip())
-            except ValueError as e:
-                amount = 0.0
-            description, category = description.strip(), category.strip()
             for expense in self.expenses:
                 if expense["id"] == expense_id:
+                    if description != "":
+                        description = description.strip()
+                    else:
+                        description = expense["description"]
+                    if category != "":
+                        category = category.strip()
+                    else:
+                        category = expense["category"]
+                    try:
+                        if amount != "":
+                            if float(amount) > 0.0:
+                                amount = float(amount.strip())
+                            else:
+                                amount = 0.0
+                        else:
+                            amount = expense["amount"]
+                    except ValueError as e:
+                            amount = 0.0
+
                     expense["amount"] = amount
                     expense["description"] = description
                     expense["category"] = category
                     self.save_data()
-                    os.system("cls")
+                    #os.system("cls")
                     print(f"Expense with the ID {expense["id"]} is updated.")
-                    print("New List\n" + "-"*50)
-                    tracker.list_expenses()
+                    print("New List\n" + "-"*75)
+                    tracker.list_expenses("")
                     return
             os.system("cls")
             print("Couldn't find an expense with the given ID.")
@@ -101,8 +162,8 @@ class ExpenseTracker:
         
         try:
             os.system("cls")
-            print("Old List\n" + "-"*50)
-            tracker.list_expenses()
+            print("Old List\n" + "-"*75)
+            tracker.list_expenses("")
             id = int(input("Please enter the ID: "))
             for expense in self.expenses:
                 if expense["id"] == id:
@@ -115,8 +176,8 @@ class ExpenseTracker:
                             if expense["id"] == i:
                                 expense["id"] = expense["id"] - 1
                             self.save_data()
-                    print("New List\n" + "-"*50)
-                    tracker.list_expenses()
+                    print("New List\n" + "-"*75)
+                    tracker.list_expenses("")
                     return
             os.system("cls")
             print(f"Couldn't find an expense with the ID {id}.")
@@ -127,8 +188,11 @@ class ExpenseTracker:
     def help(self):
         functions = [
             "Add Expense:\n  Adds a new expense entry with an amount, category, and description.\n  Usage: tracker.add_expense(50, 'Lunch', 'Food')\n  Steps:\n    1. Select 'Add(1)' from the main menu.\n    2. Enter 'amount', 'description', and 'category' separated by commas.\n    3. If you want to skip a value, leave it blank and continue.",
+            "-"*100,
             "List Expenses:\n  Displays all recorded expenses. If no expenses exist, it informs the user.\n  Usage: tracker.list_expenses()\n  Steps:\n    1. Select 'List(2)' from the main menu.\n    2. The list of expenses will be displayed in order.",
+            "-"*100,
             "Update Expense:\n  Modifies an existing expense by its ID. You can update one or multiple fields (amount, category, description).\n  Usage: tracker.update_expense(1, amount=55, description='Fruit shopping', category='Groceries')\n  Steps:\n    1. Select 'Update(3)' from the main menu.\n    2. Enter the ID of the expense you want to update.\n    3. Specify new values (leave blank to keep the old ones).\n    4. Confirm the update.",
+            "-"*100,
             "Delete Expense:\n  Removes an expense entry permanently by its ID.\n  Usage: tracker.delete_expense(1)\n  Steps:\n    1. Select 'Delete(4)' from the main menu.\n    2. Enter the ID of the expense you want to delete.\n    3. Confirm deletion.",
         ]
         os.system("cls")
@@ -137,26 +201,26 @@ class ExpenseTracker:
 tracker = ExpenseTracker()
 i = ""
 while i != "0":
-    deger = input("\nAdd(1) / List(2) / Update(3) / Delete(4) / Help(5) / Exit(0)\nPlease chose what to do: ")
-    if deger == "1":
+    data = input("\nAdd(1) / List(2) / Update(3) / Delete(4) / Help(5) / Exit(0)\nPlease chose what to do: ")
+    if data == "1":
         tracker.add_expense()
         
-    elif deger == "2":
+    elif data == "2":
         os.system("cls")
-        print("Current List\n" + "-"*50)
-        tracker.list_expenses()
+        print("Current List\n" + "-"*75)
+        tracker.list_expenses(None)
         
-    elif deger == "3":
+    elif data == "3":
         tracker.update_expense()
 
-    elif deger == "4":
+    elif data == "4":
         tracker.delete_expense()
 
-    elif deger == "5":
+    elif data == "5":
         tracker.help()
 
-    elif deger == "0":
+    elif data == "0":
         i = "0"
 
     else:
-        print(f"Error! Entered an unknown command: '{deger}'")
+        print(f"Error! Entered an unknown command: '{data}'")
